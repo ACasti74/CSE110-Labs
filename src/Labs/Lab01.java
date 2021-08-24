775ad2e1f9a7ea7453403c592aeb2a2e81fd12f3
Alejandro Castillo <CastAl02@risk.regn.net>
2021-08-24 12:09:58 -0400

    Lab 01 Work

---
+++ src/Labs/Lab01.java
@@ -0,0 +1,38 @@
+package Labs;
+
+import java.util.Scanner;
+
+/**
+ * Author: Alejandro Castillo
+ * FileName: Lab01.java
+ * Specification: Program that calculate average of tests taken
+ * For: CSE 110 - Lab #1
+ * Time Spent: 30 Minutes
+ */
+
+public class Lab01 {
+
+    public static void main(String[] args) {
+        double test1 = 0;
+        double test2 = 0;
+        double test3 = 0;
+        final double numberOfTests = 3.0;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter the score on the first test:" );
+        test1 = keyboard.nextDouble();
+        System.out.println("Your Test 1 score is: " + test1);
+        System.out.println("Enter the score on the second test:" );
+        test2 = keyboard.nextDouble();
+        System.out.println("Your Test 2 score is: " + test2);
+        System.out.println("Enter the score on the third test:" );
+        test3 = keyboard.nextDouble();
+        System.out.println("Your Test 3 score is: " + test3);
+
+        double average = (test1 + test2 + test3) / numberOfTests;
+        System.out.println("Your average score is:" + average);
+
+
+    }
+}

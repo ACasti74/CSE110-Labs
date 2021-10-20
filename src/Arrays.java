348596b0447c296a7db3d927901d8922d9fa38d5
Alejandro Castillo <Alejandro.Castillo@lexisnexisrisk.com>
2021-10-19 21:30:19 -0400

    Woking on Lab 7

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,31 +1,54 @@
 public class Arrays {
 
     public static void main(String[] args) {
-        int index = 0;
         String [] stringArray = {"Alex", "Pedro", "Juan", "Alex"};
         int [] nums = {1,2,3,12,8,12,9,15,12};
+        arrayOfInts(nums);
+        findStringInArray(stringArray, "Pedro");
+        duplicatesInArray(nums);
+    }
 
-        int arrayOfStringslength = stringArray.length;
-        System.out.println(arrayOfStringslength);
+    /**
+     * Method that finds duplicates elements of type int on an array
+     */
+    public static int duplicatesInArray(int [] arr) {
+        int duplicates = 0;
+        int firstIndex;
+        int secondIndex;
 
-        int arrayOfNumsLength = nums.length;
-        System.out.println(arrayOfNumsLength);
-
-        for (index = 0; index < arrayOfStringslength; index++) {
-            if (stringArray[index].equals("Alex")) {
-                System.out.println("Inside the array stringArray the name Alex is at index: " + index);
+        for (firstIndex = 0; firstIndex < arr.length; firstIndex++) {
+            for (secondIndex = firstIndex + 1; secondIndex < arr.length; secondIndex++) {
+                if (arr[firstIndex] == arr[secondIndex] ) {
+                    duplicates = duplicates + 1;
+                }
             }
         }
+        System.out.println("There are " + duplicates + " duplicates inside of the array");
+        return duplicates;
+    }
 
-        while (index < arrayOfNumsLength) {
-            if (nums[index] == 12) {
-                System.out.println("Inside the array nums 12 is at index: " + index);
-            }
-            index++;
+    /**
+     * Method that prints int elements inside of an array
+     */
+    public static int arrayOfInts(int[] arr) {
+        int index;
+        int arrayOfIntegers = 0;
+        for (index = 0; index < arr.length; index++) {
+            arrayOfIntegers = arr[index];
+            System.out.print(arr[index]);
         }
+        return arrayOfIntegers;
+    }
 
-        for (index = 0; index < nums.length; index++) {
-            System.out.print(nums[index]);
+    /**
+     * Method that finds an specific String inside of an array
+     */
+    public static void findStringInArray(String[] arr, String element) {
+        int index;
+        for (index = 0; index < arr.length; index++) {
+            if (arr[index].equals(element)) {
+                System.out.println("\nInside the array the element " + element +  " is at index: " + index);
+            }
         }
     }
 
